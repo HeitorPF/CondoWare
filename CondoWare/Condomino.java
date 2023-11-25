efd86384ce8e74d70b33a5681827cfb2e31d0a8c
JoaoVitor49 <joao.pimentel2017@outlook.com>
2023-11-25 17:50:42 -0300

    Tela de Login

---
+++ CondoWare/Condomino.java
@@ -0,0 +1,20 @@
+package condoware.CondoWare;
+
+public class Condomino extends Pessoa {
+    
+    private int numAp;
+
+    public int getNumAp() {
+        return numAp;
+    }
+
+    public void setNumAp(int numAp) {
+        this.numAp = numAp;
+    }
+    
+    public Veiculo addVeiculo(String placa, String modelo, String marca) {
+        
+    }
+    
+   
+}
