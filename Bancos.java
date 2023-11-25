be311fa72eabac9fec1c64a4908389f64e9aa178
HeitorPF <heitorpf12.hf@gmail.com>
2023-11-25 18:00:08 -0300

    resolverndo merge

---
+++ Bancos.java
@@ -0,0 +1,10 @@
+package condoware.CondoWare;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Bancos {
+    private static Bancos bancoUnico;
+    
+    static List<Condomino> bdCondomino;
+}
