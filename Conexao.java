7b233a4b4dcbb23bbb3bf0452a8c8f60cf81c0c2
HeitorPF <heitorpf12.hf@gmail.com>
2023-11-26 14:59:31 -0300

    conexao

---
+++ Conexao.java
@@ -0,0 +1,39 @@
+package condoware.CondoWare;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Conexao {
+    
+    private String url;
+    private String usuario;
+    private String senha;
+    private Connection con;
+    private static Conexao conUnico;
+    
+    private static Conexao getConexao() {
+        if(conUnico == null) {
+            conUnico = new Conexao(senha);
+        }
+        return conUnico;
+    }
+    
+    private Conexao(String senha) {
+        url = "jdbc:postgresql://localhost:5432/Sapaiada";
+        usuario = "postgres";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(url, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Conexão feita com sucesso!",
+                    "Sucesso", 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
