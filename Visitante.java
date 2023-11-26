b540b1b34e395cd2dfdaf79adb71215799c3822d
HeitorPF <heitorpf12.hf@gmail.com>
2023-11-26 14:27:12 -0300

    visitante

---
+++ Visitante.java
@@ -0,0 +1,49 @@
+package condoware.CondoWare;
+
+public class Visitante {
+    private String entrada;
+    private String blocoVisitado;
+    private String nomeVisitante;
+    private String apVisitado;
+    private String saida;
+
+    public String getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(String entrada) {
+        this.entrada = entrada;
+    }
+
+    public String getBlocoVisitado() {
+        return blocoVisitado;
+    }
+
+    public void setBlocoVisitado(String blocoVisitado) {
+        this.blocoVisitado = blocoVisitado;
+    }
+
+    public String getNomeVisitante() {
+        return nomeVisitante;
+    }
+
+    public void setNomeVisitante(String nomeVisitante) {
+        this.nomeVisitante = nomeVisitante;
+    }
+
+    public String getApVisitado() {
+        return apVisitado;
+    }
+
+    public void setApVisitado(String apVisitado) {
+        this.apVisitado = apVisitado;
+    }
+
+    public String getSaida() {
+        return saida;
+    }
+
+    public void setSaida(String saida) {
+        this.saida = saida;
+    }
+}
