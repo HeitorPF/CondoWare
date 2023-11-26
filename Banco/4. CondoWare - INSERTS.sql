2bf1c1f34d580df822c172e4a440a30026fcb928
Mamamarcelo <102704462+Mamamarcelo@users.noreply.github.com>
2023-11-26 16:06:51 -0300

    new file:   Banco/1. CondoWare - CREATES.sql 	new file:   Banco/2. CondoWare - VIEWS, FUNCTIONS E TRIGGERS.sql 	new file:   Banco/3. CondoWare - ROLE.sql 	new file:   Banco/4. CondoWare - INSERTS.sql

---
+++ Banco/4. CondoWare - INSERTS.sql
@@ -0,0 +1,46 @@
+INSERT INTO Condominio VALUES ('Condominio Manauara', 'Rua das Lamentações, 60, Jd. Porto Bello', 15, 30);
+
+INSERT INTO Pessoa VALUES (12345678900, 'Algusto Barreto Costa', 'sindico@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (23456789001, 'Desmond Eixo Feitosa', 'desmond@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (45678900123, 'Gustavo Heitor Iano', 'gustavo@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (67890012345, 'João Kamilo Lorenzote', 'joao@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (89001234567, 'Marcos Neto Oppenheimer', 'marcos@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (34567890012, 'Antônio Gomes Pinto', 'antonio@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (90123456789, 'Paula Queiroz Rossi', 'paula@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (91234567890, 'Laura Silva Costa', 'laura@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (56789001234, 'Brenda Solom Bastos', 'brenda@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (78900123456, 'Sofia Ramos Evergreen', 'sofia@gmail.com', 'root');
+INSERT INTO Pessoa VALUES (90012345678, 'Matheus Souza Batista', 'matheus@gmail.com', 'root');
+
+INSERT INTO Funcionario VALUES (12345678900, 4000.00, 'Síndico');
+INSERT INTO Funcionario VALUES (23456789001, 2500.00, 'Zelador');
+INSERT INTO Funcionario VALUES (45678900123, 3000.00, 'Porteiro');
+INSERT INTO Funcionario VALUES (67890012345, 3000.00, 'Porteiro');
+INSERT INTO Funcionario VALUES (89001234567, 2500.00, 'Zelador');
+
+INSERT INTO Condomino VALUES (12345678900, 1, 1);
+INSERT INTO Condomino VALUES (34567890012, 1, 20);
+INSERT INTO Condomino VALUES (90123456789, 1, 5);
+INSERT INTO Condomino VALUES (91234567890, 1, 30);
+INSERT INTO Condomino VALUES (56789001234, 4, 8);
+INSERT INTO Condomino VALUES (78900123456, 5, 10);
+INSERT INTO Condomino VALUES (90012345678, 15, 20);
+
+INSERT INTO Veiculo VALUES (12345678900, 'ABC-123');
+INSERT INTO Veiculo VALUES (23456789001, 'XYZ-987');
+INSERT INTO Veiculo VALUES (45678900123, 'DEF-456');
+INSERT INTO Veiculo VALUES (67890012345, 'GHI-789');
+INSERT INTO Veiculo VALUES (34567890012, 'JKL-012');
+INSERT INTO Veiculo VALUES (90123456789, 'MNO-345');
+INSERT INTO Veiculo VALUES (91234567890, 'PQR-678');
+INSERT INTO Veiculo VALUES (56789001234, 'STU-901');
+INSERT INTO Veiculo VALUES (78900123456, 'VWX-234');
+INSERT INTO Veiculo VALUES (78900123456, 'YZA-567');
+
+INSERT INTO Area_Lazer VALUES (101, 'Espaço Festa', 70, 100, FALSE, NULL, NULL);
+INSERT INTO Area_Lazer VALUES (102, 'Espaço com Churrasqueira', 40, 70, TRUE, '2023-12-20', 12345678900);
+INSERT INTO Area_Lazer VALUES (401, 'Espaço Festa', 100, 100, FALSE, NULL, NULL);
+INSERT INTO Area_Lazer VALUES (402, 'Piscina', 50, 100, TRUE, '2023-9-10', 90123456789);
+INSERT INTO Area_Lazer VALUES (403, 'Espaço Festa', 50, 70, FALSE, NULL, NULL);
+INSERT INTO Area_Lazer VALUES (1101, 'Espaço com Churrasqueira', 40, 70, TRUE, '2025-10-19', 56789001234);
+INSERT INTO Area_Lazer VALUES (1102, 'Piscina', 50, 100, FALSE, NULL, NULL);
