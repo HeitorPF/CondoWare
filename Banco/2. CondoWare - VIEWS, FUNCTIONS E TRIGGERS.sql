c12d7bdb71b48decfa5e0dbe3041cb4f213b686f
Mamamarcelo <102704462+Mamamarcelo@users.noreply.github.com>
2023-11-28 17:39:07 -0300

    modified:   Banco/2. CondoWare - VIEWS, FUNCTIONS E TRIGGERS.sql

--- Banco/2. CondoWare - VIEWS, FUNCTIONS E TRIGGERS.sql
+++ Banco/2. CondoWare - VIEWS, FUNCTIONS E TRIGGERS.sql
@@ -1,3 +1,78 @@
+--FUNÇÃO P/ CRIAR OS APARTAMENTOS ASSIM QUE UM CONDOMÍNIO FOR CRIADO NA TABELA CONDOMÍNIO
+CREATE OR REPLACE FUNCTION tab_apartamentos() RETURNS TRIGGER AS $$
+DECLARE
+	bloco INTEGER := NEW.quant_blocos::INTEGER;
+	ap INTEGER := NEW.quant_ap_bloco::INTEGER;
+BEGIN
+	FOR contBloco in 1..bloco LOOP
+		FOR contAp in 1..ap LOOP
+			INSERT INTO Apartamento VALUES(contBloco, contAp, FALSE);
+		END LOOP;
+	END LOOP;
+	RETURN NEW;
+END; $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insere_apartamentos AFTER INSERT ON Condominio
+FOR EACH ROW EXECUTE PROCEDURE tab_apartamentos();
+
+--FUNÇÃO PARA MARCAR UM AP COMO OCUPADA SEMPRE QUE UM CONDÔMINO FOR REGISTRADO
+CREATE OR REPLACE FUNCTION ocup_ap() RETURNS TRIGGER AS $$
+BEGIN
+	UPDATE Apartamento SET ocupado = TRUE WHERE bloco = NEW.bloco AND numero = NEW.apartamento;
+	RETURN NEW;
+END; $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER muda_status AFTER INSERT ON Condomino
+FOR EACH ROW EXECUTE PROCEDURE ocup_ap();
+
+--FUNÇÃO P/ QUANDO FOR REALIZADO LOGIN INFORMAR O TIPO DE USUÁRIO QUE ESTÁ LOGANDO
+CREATE OR REPLACE FUNCTION desc_pessoa(mail VARCHAR) RETURNS INTEGER AS $$
+DECLARE
+	doc BIGINT;
+BEGIN
+	IF EXISTS (SELECT * FROM Pessoa WHERE mail = Pessoa.email) THEN
+		doc := (SELECT Pessoa.cpf FROM Pessoa WHERE mail = Pessoa.email LIMIT 1) AS id;
+		IF EXISTS (SELECT * FROM Condomino WHERE Condomino.cpf = doc) THEN
+			IF EXISTS (SELECT * FROM Funcionario WHERE Funcionario.cpf = doc AND cargo = 'Síndico') THEN
+				RETURN 1;
+			ELSEIF EXISTS (SELECT * FROM Funcionario WHERE Funcionario.cpf = doc) THEN
+				RETURN 2;
+			ELSE
+				RETURN 3;
+			END IF;
+		ELSEIF EXISTS (SELECT * FROM Funcionario WHERE Funcionario.cpf = doc) THEN
+			RETURN 2;
+		END IF;
+	END IF;
+	RETURN 0;
+END; $$ LANGUAGE plpgsql;
+
+--REGISTRA ENTRADA DE UM VISITANTE
+CREATE OR REPLACE FUNCTION visita_entrada(nom VARCHAR, bloc INTEGER, ap INTEGER) RETURNS VOID AS $$
+BEGIN
+	INSERT INTO Visita VALUES (nom, bloc, ap, CURRENT_TIMESTAMP, NULL);
+END; $$ LANGUAGE plpgsql;
+
+--REGISTRA A SAÍDA DO VISITANTE
+CREATE OR REPLACE FUNCTION visita_saida(nom VARCHAR) RETURNS VOID AS $$
+BEGIN
+	UPDATE Visita SET saida = CURRENT_TIMESTAMP WHERE nome = nom;
+END; $$ LANGUAGE plpgsql;
+
+--RELATÓRIO DE VISITANTES
+CREATE VIEW hist_visitas (nome, condomino, bloco, apartamento, entrada, saida) AS
+SELECT nome, (SELECT nome
+			  FROM (SELECT * 
+					FROM Pessoa NATURAL JOIN Condomino WHERE bloco = bloco_vis AND apartamento = apartamento_vis) AS cond LIMIT 1) AS condomino, 
+bloco_vis, apartamento_vis, entrada, saida
+FROM Visita;
+
+--RELATÓRIO DOS PROBLEMAS
+CREATE VIEW relatorio_problemas(nome, descricao, bloco, data) AS
+SELECT (SELECT nome FROM PESSOA WHERE condomino = cpf LIMIT 1) AS nome, descricao, bloco, data
+FROM Reg_Problemas NATURAL JOIN Pessoa
+WHERE condomino = cpf;
+
 --RELATÓRIO E INSERIR FUNCIONÁRIO
 CREATE VIEW relatorio_funcionarios(cpf, nome, salario, cargo, placa, email, senha) AS
 SELECT Pessoa.cpf, nome, salario, cargo, (SELECT placa FROM Veiculo WHERE Pessoa.cpf = Veiculo.cpf LIMIT 1) AS placa, email, senha
@@ -86,13 +161,6 @@ BEGIN
 	END IF;
 END; $$ LANGUAGE plpgsql;
 
---RELATÓRIO DAS ÁREAS ALUGADAS
-CREATE VIEW relatorio_areas_alugadas(id, tipo, data, locador, bloco, apartamento) AS
-SELECT id, tipo, data_aluguel, nome, (SELECT bloco FROM Condomino WHERE locador = cpf LIMIT 1) as bloco, 
-	(SELECT apartamento FROM Condomino WHERE locador = cpf LIMIT 1) as apartamento
-FROM Area_Lazer NATURAL JOIN Pessoa
-WHERE alugada = TRUE AND locador = cpf;
-
 --FUNÇÃO PARA ALUGAR ÁREAS DE LAZER
 CREATE OR REPLACE FUNCTION alug_area(cod INTEGER, dat DATE, loc BIGINT) RETURNS INTEGER AS $$
 BEGIN
@@ -109,78 +177,3 @@ CREATE VIEW areas_alugadas(id, tipo, data, locador) AS
 SELECT id, tipo, data_aluguel, (SELECT nome FROM Pessoa WHERE cpf = locador) AS locador
 FROM Area_Lazer NATURAL JOIN Areas_Alug
 WHERE Area_Lazer.id = Areas_Alug.id
-
---FUNÇÃO P/ CRIAR OS APARTAMENTOS ASSIM QUE UM CONDOMÍNIO FOR CRIADO NA TABELA CONDOMÍNIO
-CREATE OR REPLACE FUNCTION tab_apartamentos() RETURNS TRIGGER AS $$
-DECLARE
-	bloco INTEGER := NEW.quant_blocos::INTEGER;
-	ap INTEGER := NEW.quant_ap_bloco::INTEGER;
-BEGIN
-	FOR contBloco in 1..bloco LOOP
-		FOR contAp in 1..ap LOOP
-			INSERT INTO Apartamento VALUES(contBloco, contAp, FALSE);
-		END LOOP;
-	END LOOP;
-	RETURN NEW;
-END; $$ LANGUAGE plpgsql;
-
-CREATE TRIGGER insere_apartamentos AFTER INSERT ON Condominio
-FOR EACH ROW EXECUTE PROCEDURE tab_apartamentos();
-
---FUNÇÃO PARA MARCAR UM AP COMO OCUPADA SEMPRE QUE UM CONDÔMINO FOR REGISTRADO
-CREATE OR REPLACE FUNCTION ocup_ap() RETURNS TRIGGER AS $$
-BEGIN
-	UPDATE Apartamento SET ocupado = TRUE WHERE bloco = NEW.bloco AND numero = NEW.apartamento;
-	RETURN NEW;
-END; $$ LANGUAGE plpgsql;
-
-CREATE TRIGGER muda_status AFTER INSERT ON Condomino
-FOR EACH ROW EXECUTE PROCEDURE ocup_ap();
-
---FUNÇÃO P/ QUANDO FOR REALIZADO LOGIN INFORMAR O TIPO DE USUÁRIO QUE ESTÁ LOGANDO
-CREATE OR REPLACE FUNCTION desc_pessoa(mail VARCHAR) RETURNS INTEGER AS $$
-DECLARE
-	doc BIGINT;
-BEGIN
-	IF EXISTS (SELECT * FROM Pessoa WHERE mail = Pessoa.email) THEN
-		doc := (SELECT Pessoa.cpf FROM Pessoa WHERE mail = Pessoa.email LIMIT 1) AS id;
-		IF EXISTS (SELECT * FROM Condomino WHERE Condomino.cpf = doc) THEN
-			IF EXISTS (SELECT * FROM Funcionario WHERE Funcionario.cpf = doc AND cargo = 'Síndico') THEN
-				RETURN 1;
-			ELSEIF EXISTS (SELECT * FROM Funcionario WHERE Funcionario.cpf = doc) THEN
-				RETURN 2;
-			ELSE
-				RETURN 3;
-			END IF;
-		ELSEIF EXISTS (SELECT * FROM Funcionario WHERE Funcionario.cpf = doc) THEN
-			RETURN 2;
-		END IF;
-	END IF;
-	RETURN 0;
-END; $$ LANGUAGE plpgsql;
-
---REGISTRA ENTRADA DE UM VISITANTE
-CREATE OR REPLACE FUNCTION visita_entrada(nom VARCHAR, bloc INTEGER, ap INTEGER) RETURNS VOID AS $$
-BEGIN
-	INSERT INTO Visita VALUES (nom, bloc, ap, CURRENT_TIMESTAMP, NULL);
-END; $$ LANGUAGE plpgsql;
-
---REGISTRA A SAÍDA DO VISITANTE
-CREATE OR REPLACE FUNCTION visita_saida(nom VARCHAR) RETURNS VOID AS $$
-BEGIN
-	UPDATE Visita SET saida = CURRENT_TIMESTAMP WHERE nome = nom;
-END; $$ LANGUAGE plpgsql;
-
---RELATÓRIO DE VISITANTES
-CREATE VIEW hist_visitas (nome, condomino, bloco, apartamento, entrada, saida) AS
-SELECT nome, (SELECT nome
-			  FROM (SELECT * 
-					FROM Pessoa NATURAL JOIN Condomino WHERE bloco = bloco_vis AND apartamento = apartamento_vis) AS cond LIMIT 1) AS condomino, 
-bloco_vis, apartamento_vis, entrada, saida
-FROM Visita;
-
---RELATÓRIO DOS PROBLEMAS
-CREATE VIEW relatorio_problemas(nome, descricao, bloco, data) AS
-SELECT (SELECT nome FROM PESSOA WHERE condomino = cpf LIMIT 1) AS nome, descricao, bloco, data
-FROM Reg_Problemas NATURAL JOIN Pessoa
-WHERE condomino = cpf;
